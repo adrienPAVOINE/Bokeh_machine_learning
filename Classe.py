7d2d49274eb87a29ea42a88962e68865b86b5bc2
ameliepcrd <49780805+ameliepcrd@users.noreply.github.com>
2020-11-15 19:59:45 +0100

    Update Classe.py

--- Classe.py
+++ Classe.py
@@ -11,12 +11,9 @@ from sklearn.tree import plot_tree
 from sklearn.tree import export_text
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn import preprocessing
 from sklearn import model_selection
 import matplotlib.pyplot as plt
-import statsmodels as sm
-from statsmodels.tools import add_constant
-from statsmodels.api import Logit
-import scipy
 import numpy
 import pandas
 
@@ -46,6 +43,7 @@ class Algo_Var_Cat():
         #print(self.df)
         
         #distribution des classes
+        print("Distribution des classes :")
         print(self.yTrain.value_counts(normalize=True))
         print(self.yTest.value_counts(normalize=True))
         print(self.yTrain,self.XTrain)
@@ -99,15 +97,14 @@ class Algo_Var_Cat():
         #matrice de confusion
         #Afficher la matrice de confusion !
         mc = metrics.confusion_matrix(self.yTest,yPred)
-        print(mc)
+        print("Matrice de confusion :", mc)
         
         #taux de reconnaissance
         acc = metrics.accuracy_score(self.yTest,yPred)
-        print(acc)
+        print("Taux de reconnaissance : %.4f" % acc)
         
-        #taux d'erreur
-        err = 1.0 - acc
-        print(err)
+        #calcul du taux d'erreur
+        print("Taux d'erreur :%.4f" % 1.0-metrics.accuracy_score(self.yTest,yPred))
         
         #rappel par classe
         print(metrics.recall_score(self.yTest,yPred,average=None))
@@ -116,6 +113,7 @@ class Algo_Var_Cat():
         print(metrics.precision_score(self.yTest,yPred,average=None))
         
         #rapport général
+        print("Rapport sur la qualité de prédiction : ")
         print(metrics.classification_report(self.yTest,yPred))
         
         #-------------------------------------------------------------------------
@@ -125,10 +123,10 @@ class Algo_Var_Cat():
         # paramètre par défaut : nb_cross_val=10
         succes = model_selection.cross_val_score(dtree,self.X,self.y,cv=nb_cross_val,scoring='accuracy')
         #détail des itérations
-        print(succes)
+        print("Succès de la validation croisée :", succes)
         #moyenne des taux de succès = estimation du taux de succès en CV
-        print(succes.mean())
-        
+        print("Moyenne des succès : %.4f " % succes.mean()) 
+
     #-------------------------------------------------------------------------
     #Création de l'analyse discrinimante linéaire
     #-------------------------------------------------------------------------  
@@ -149,20 +147,26 @@ class Algo_Var_Cat():
         tmp2=pandas.DataFrame(tmp2)
         tmp2=tmp2.rename(index={0 : "Constante"})
         final=pandas.concat([tmp2,tmp])
-        print(final)
+        print("Table des coefficients et des intercepts : ",final)
                 
         #-------------------------------------------------------------------------
         #Prédiction : 
         #-------------------------------------------------------------------------
         ypred = lda.predict(self.XTest)
         #matrice de confusion
-        mc = metrics.confusion_matrix(self.yTest,ypred)
-        print(mc)
-        
+        mc=pandas.crosstab(self.yTest,ypred)
+        print("Matrice de confusion : ",mc)
+        #transformer en matrice Numpy
+        mcSmNumpy = mc.values
+        #taux de reconnaissance
+        accSm = numpy.sum(numpy.diagonal(mcSmNumpy))/numpy.sum(mcSmNumpy)
+        print("Taux de reconnaissance : %.4f" % (accSm))
+                
         #calcul du taux d'erreur
-        print(1.0-metrics.accuracy_score(self.yTest,ypred))
+        print("Taux d'erreur :" , 1.0-metrics.accuracy_score(self.yTest,ypred))
         
         #calcul des sensibilité (rappel) et précision par classe
+        print("Rapport sur la qualité de prédiction : ")
         print(metrics.classification_report(self.yTest,ypred))
         
         #-------------------------------------------------------------------------
@@ -172,174 +176,101 @@ class Algo_Var_Cat():
         # paramètre par défaut : nb_cross_val=10
         succes = model_selection.cross_val_score(lda,self.X,self.y,cv=nb_cross_val,scoring='accuracy')
         #détail des itérations
-        print(succes)
+        print("Succès de la validation croisée :", succes)
         #moyenne des taux de succès = estimation du taux de succès en CV
-        print(succes.mean())
+        print("Moyenne des succès : %.4f " % succes.mean()) 
 
 
     #-------------------------------------------------------------------------
     #Création de la regressionn logistiques binaire
     #-------------------------------------------------------------------------  
-    def Regression_log(self,alpha=0.1,nb_cross_val=10):
-        #rajout d'une constante de valeur 1 dans la première colonne
-        XTrainBis = sm.tools.add_constant(self.XTrain)
-        #régression logistique - on passe la cible et les explicatives
-        lr = Logit(endog=self.yTrain,exog=XTrainBis)
-        #algorithme de Newton-Raphson utilisé par défaut
-        res = lr.fit() 
+    def Regression_log(self,multi,alpha=0.1,nb_cross_val=10):
         
-        print("Résumé des résultats")
-        print(res.summary()) 
+        #instanciation
+        stds = preprocessing.StandardScaler()
+        #transformation centrer et reduire
+        ZTrain = stds.fit_transform(self.XTrain)
         
-        print("Intervalle de confiance des coefficients à %.i pourcent" % ((1-alpha)*100))
-        print(res.conf_int(alpha=alpha))
-         
-        print("Voici les coefficients estimés")
-        print(res.params)
-         
-        print("Matrice de confusion")
-        print(res.pred_table()) 
+        if (multi==True) :
+            #instanciation
+            lrSkStd = LogisticRegression(penalty='none', multi_class='multinomial')
+        elif (multi==False):
+            #instanciation
+            lrSkStd = LogisticRegression(penalty='none')
         
-        print("#Pseudo-R2 :")
-        print("Log-vraisemblance du modèle : %.4f" % (res.llf))
+        #lancement des calculs -- pas nécessaire de rajouter la constante
+        lrSkStd.fit(ZTrain,self.yTrain)
         
-        #log-vraisemblance du null modèle
-        print("Log-vraisemblance du null modèle : %.4f" % (res.llnull))
-        #R2 de McFadden
-        print("R2 de McFadden : %.4f" % (res.prsquared))
-        #exponenielle de LL_null
-        L0 = numpy.exp(res.llnull)
-        #exponentielle de LL_modèle
-        La = numpy.exp(res.llf)
-        #taille de l'échantillon
-        n = self.XTrain.shape[0]
-        #R2 de Cox et Snell
-        R2CS = 1.0 - (L0 / La)**(2.0/n)
-        print("R2 de Cox - Snell : %.4f" % (R2CS))
-        #max du R2 de COx-Snell
-        maxR2CS = 1.0 - (L0)**(2.0/n)
-        #R2 de Nagelkerke
-        R2N = R2CS / maxR2CS
-        print("R2 de Nagelkerke : %.4f" % (R2N))
+        #correction des coefficients - dé-standardisation
+        #par les écarts-type utilisés lors de la standardisation des variables
+        coefUnstd = lrSkStd.coef_[0] / stds.scale_
+        #affichage des coefficients corrigés
+        print(pandas.DataFrame({"var":self.XTrain.columns,"coef":coefUnstd}))
+        #pour la constante, l'opération est plus complexe
+        interceptUnStd = lrSkStd.intercept_ + numpy.sum(lrSkStd.coef_[0]*(-stds.mean_/stds.scale_))
+        print("Constante(s) :",interceptUnStd)
         
-        print("#Evaluation basés sur les scores")
-        # On se base sur les scores - c.-à-d. probabilité d'affectation à la modalité cible - pour évaluer la qualité d'approximation du modèle. Il faut déjà les calculer.
-        #scores fournis par la régression pour chaque individus
-        scores = lr.cdf(res.fittedvalues)        
+        if (multi==False):
+            #Le log-vraisemblance
+            #probabilités d'affectation
+            proba01 = lrSkStd.predict_proba(ZTrain)
+            #récupération de la colonne n°1
+            proba1 = proba01[:,1]
+            #log-vraisemblance
+            log_likelihood = numpy.sum(self.yTrain*numpy.log(proba1)+(1.0-self.yTrain)*numpy.log(1.0-proba1))
+            print("La log-vraisemblance vaut :",log_likelihood)
+            
         
-        ### Diagramme de fiabilité
-        #data frame temporaire avec y et les scores
-        df = pandas.DataFrame({"y":self.yTrain,"score":scores})
-        #5 intervalles de largeur égales
-        intv = pandas.cut(df.score,bins=5,include_lowest=True)
-        #intégrées dans le df
-        df['intv'] = intv
-           
-        print("Moyenne des scores par groupe :")
-        m_score = df.pivot_table(index="intv",values="score",aggfunc="mean")
-        print(m_score)
-        print("Moyenne des y - qui équivaut à une proportion puisque 0/1")
-        m_y = df.pivot_table(index="intv",values="y",aggfunc="mean")
-        print(m_y)
-        
-        #construire la diagonale
-        plt.plot(numpy.arange(0,1,0.1),numpy.arange(0,1,0.1),'b')
-        #rajouter notre diagramme
-        plt.plot(m_score,m_y,"go-")
-        #titre
-        plt.title("Diagramme de fiabilité")
-        #faire apparaître
-        plt.show()
-        
-        print("#Test de Hosmer-Lemeshow : ")
-        #effectifs par groupe
-        n_tot = df.pivot_table(index="intv",values="y",aggfunc="count").values[:,0]
-        #somme des scores par groupes
-        s_scores = df.pivot_table(index='intv',values="score",aggfunc="sum").values[:,0]
-        #nombre de positifs par groupe
-        n_pos = df.pivot_table(index="intv",values="y",aggfunc="sum").values[:,0]
-        #nombre de négatifs par groupe
-        n_neg = n_tot - n_pos
-        #statistique de Hosmer-Lemeshow
-        C1 = numpy.sum((n_pos - s_scores)**2/s_scores)
-        C2 = numpy.sum((n_neg - (n_tot - s_scores))**2/((n_tot - s_scores)))
-        HL = C1 + C2
-        print("Statistique de Hosmer-Lemeshow : %.4f  " % (HL))
-        #probabilité critique
-        pvalue = 1.0 - scipy.stats.chi2.cdf(HL,8)
-        print("p-value : %.4f " % (pvalue))
-        
-        print("Tests de significativité des coefficients : ")
-        #AIC du modèle
-        print("AIC du modèle : %.4f" % (res.aic))
-        #AIC du modèle trivial - 1 seul param. estimé, la constante
-        aic_null = (-2) * res.llnull + 2 * (1)
-        print("AIC du modèle trivial : %.4f" % (aic_null))
-        #BIC du modèle
-        print("BIC du modèle : %.4f" % (res.bic))
-        #BIC du modèle trivial - 1 seul param. estimé, la constante
-        
-        bic_null = (-2) * res.llnull + numpy.log(n) * (1)
-        print("BIC du modèle trivial : %.4f" % (bic_null))
-        
-        print("# Prédiction et matrice de confusion")
-        #préparation de l'échantillon test
-        #par adjonction de la constante
-        XTest_Bis = add_constant(self.XTest)
-        #calcul de la prédiction sur l'échantillon test
-        predProbaSm = res.predict(XTest_Bis)
-        
-        #convertir en prédiction brute
-        predSm = numpy.where(predProbaSm > 0.5, 1, 0)
-        print(numpy.unique(predSm,return_counts=True))
-        print("Nombre d'individus associé à la classe 0 : %.i" % numpy.unique(predSm,return_counts=True)[1][0])
-        print("Nombre d'individus associé à la classe 1 : %.i" %numpy.unique(predSm,return_counts=True)[1][1])
-        
-        print("Matrice de confusion")
-        mcSm = pandas.crosstab(self.yTest,predSm)
-        print(mcSm)
-        
+        #-------------------------------------------------------------------------
+        #Prédiction : 
+        #-------------------------------------------------------------------------
+        #transformation de l'échantillon test (centrer-réduire)
+        ZTest = stds.transform(self.XTest)
+        #appliquer la prédiction
+        predSk = lrSkStd.predict(ZTest)
+        #matrice de confusion
+        mcSm=pandas.crosstab(self.yTest,predSk)
+        print("Matrice de confusion :",mcSm)
         #transformer en matrice Numpy
         mcSmNumpy = mcSm.values
-        
-        #Indicateur de performance :
-        print("Indicateur de performance :")
         #taux de reconnaissance
         accSm = numpy.sum(numpy.diagonal(mcSmNumpy))/numpy.sum(mcSmNumpy)
         print("Taux de reconnaissance : %.4f" % (accSm))
         #taux d'erreur
         errSm = 1.0 - accSm
-        print("Taux d'erreur : %.4f" % (errSm))
+        print("Taux d'erreur' : %.4f" % (errSm))
+        #rapport sur la qualité de prédiction
+        print("Rapport sur la qualité de prédiction : ")
+        print(metrics.classification_report(self.yTest,predSk))
         
-        # ### Courbe ROC en test
-        # Construire la courbe ROC et calculer la valeur de l'AUC sur l'échantillon test. On utilise "scikit-learn" pour aller au plus court.
-        #colonnes pour les courbes ROC
-        fprSm, tprSm, _ = metrics.roc_curve(self.yTest,predProbaSm,pos_label=1)
-        
-        #graphique
-        #construire la diagonale
-        plt.plot(numpy.arange(0,1.1,0.1),numpy.arange(0,1.1,0.1),'b')
-        #rajouter notre diagramme
-        plt.plot(fprSm,tprSm,"g")
-        #titre
-        plt.title("Courbe ROC")
-        #faire apparaître
-        plt.show()
-        
-        #valeur de l'AUC
-        aucSm = metrics.roc_auc_score(self.yTest,predProbaSm)
-        print("AUC : %.4f" % (aucSm))
+        # Construire la courbe ROC et calculer la valeur de l'AUC sur l'échantillon test
+        if (multi==False):
+            #colonnes pour les courbes ROC
+            fprSm, tprSm, _ = metrics.roc_curve(self.yTest,predSk,pos_label=1)
+            
+            #graphique
+            #construire la diagonale
+            plt.plot(numpy.arange(0,1.1,0.1),numpy.arange(0,1.1,0.1),'b')
+            #rajouter notre diagramme
+            plt.plot(fprSm,tprSm,"g")
+            #titre
+            plt.title("Courbe ROC")
+            #faire apparaître
+            plt.show()
+            
+            #valeur de l'AUC
+            aucSm = metrics.roc_auc_score(self.yTest,predSk)
+            print("AUC : %.4f" % (aucSm))
         
         #-------------------------------------------------------------------------
         #Validation croisée : 
         #-------------------------------------------------------------------------
-        lr2 = LogisticRegression()
         #évaluation en validation croisée : 10 cross-validation
         # paramètre par défaut : nb_cross_val=10
-        succes = model_selection.cross_val_score(lr2,self.X,self.y,cv=nb_cross_val,scoring='accuracy')
+        succes = model_selection.cross_val_score(lrSkStd,self.X,self.y,cv=nb_cross_val,scoring='accuracy')
         #détail des itérations
-        print(succes)
+        print("Succès de la validation croisée :", succes)
         #moyenne des taux de succès = estimation du taux de succès en CV
-        print(succes.mean()) 
+        print("Moyenne des succès : %.4f " % succes.mean()) 
 
          
